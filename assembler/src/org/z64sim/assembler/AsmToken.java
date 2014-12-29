b19bfbf6739239eeb0f128c9e87d54ae6fb6e535
Alessandro Pellegrini <alessandro@pellegrini.tk>
2014-12-29 11:19:38 +0100

    Lexer correctly stores Token position in stream

--- assembler/src/org/z64sim/assembler/AsmToken.java
+++ assembler/src/org/z64sim/assembler/AsmToken.java
@@ -1,15 +1,5 @@
-/*
- * Copyright 2008 Ayman Al-Sairafi ayman.alsairafi@gmail.com
+/**
  * 
- * Licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
- * You may obtain a copy of the License 
- *       at http://www.apache.org/licenses/LICENSE-2.0 
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- * See the License for the specific language governing permissions and 
- * limitations under the License.  
  */
 package org.z64sim.assembler;
 
@@ -34,7 +24,7 @@ public class AsmToken implements Serializable, Comparable {
      * more easily. It is the JavaCC lexer that, upon the creation of a Token,
      * correctly initializes the elements of this super class as well.
      */
-    public AsmToken() {
+    protected AsmToken() {
     }
     
     public AsmToken(int kind, int start, int length) {
