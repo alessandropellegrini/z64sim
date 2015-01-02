ce2bbfc7c91db1197ff7884a85e7d9c05eaaa6bf
Alessandro Pellegrini <alessandro@pellegrini.tk>
2015-01-02 21:49:58 +0100

    Implementing the memory viewer

---
+++ memory/src/org/z64sim/memory/Memory.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.z64sim.memory;
+
+import java.util.ArrayList;
+import org.z64sim.program.MemoryElement;
+
+/**
+ *
+ * @author Alessandro Pellegrini <pellegrini@dis.uniroma1.it>
+ */
+public class Memory {
+    
+    public static ArrayList<MemoryElement> memoryMap = new ArrayList<MemoryElement>();
+    
+    
+    public static synchronized void updateMemory() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
