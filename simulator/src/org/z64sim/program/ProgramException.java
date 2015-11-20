6451505f5f4d9c8ba8b820a23db385c74d370e95
Alessandro Pellegrini <pellegrini@dis.uniroma1.it>
2015-11-20 11:12:03 +0100

    ProgramException introduced
    The ProgramException, which bridges between the Program and the
    Assembler packages, to handle errors which cannot be detected by
    the grammar, is introduced.
    
    Signed-off-by: Alessandro Pellegrini <pellegrini@dis.uniroma1.it>

---
+++ simulator/src/org/z64sim/program/ProgramException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.z64sim.program;
+
+/**
+ *
+ * @author Alessandro Pellegrini <pellegrini@dis.uniroma1.it>
+ */
+public class ProgramException extends Exception {
+    
+    public ProgramException(String msg) {
+        super(msg);
+    }
+}
