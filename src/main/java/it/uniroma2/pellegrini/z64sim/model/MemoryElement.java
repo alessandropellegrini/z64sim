778afeed8b8ccf32159a2397b6dee3de8cf07253
Alessandro Pellegrini <a.pellegrini@ing.uniroma2.it>
2022-12-14 13:51:29 +0100

    Make project REUSE comliant again
    Some new files didn't have proper licensing. This is now fixed.
    
    Signed-off-by: Alessandro Pellegrini <a.pellegrini@ing.uniroma2.it>

--- src/main/java/it/uniroma2/pellegrini/z64sim/model/MemoryElement.java
+++ src/main/java/it/uniroma2/pellegrini/z64sim/model/MemoryElement.java
@@ -1,8 +1,13 @@
+/**
+ * SPDX-FileCopyrightText: 2015-2022 Alessandro Pellegrini <a.pellegrini@ing.uniroma2.it>
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
 package it.uniroma2.pellegrini.z64sim.model;
 
 public interface MemoryElement {
 
     public byte[] getValue();
+
     public int getSize();
 
 }
