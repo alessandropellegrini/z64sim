71340ba03c79fd952def4dc6fdd8c47c571e24d4
Alessandro Pellegrini <a.pellegrini@ing.uniroma2.it>
2023-01-03 18:34:08 +0100

    Add blank to avoid label to suddenly appear
    Adding a space to the editorPositionLabel ensures that the component is
    displayed from the beginning, even when caret position is not available
    yet. This prevent the component to appear suddenly when focus is gained
    by the editor.
    
    Signed-off-by: Alessandro Pellegrini <a.pellegrini@ing.uniroma2.it>

--- src/main/java/it/uniroma2/pellegrini/z64sim/view/MainWindow.java
+++ src/main/java/it/uniroma2/pellegrini/z64sim/view/MainWindow.java
@@ -326,7 +326,7 @@ public class MainWindow extends View {
         if(tabbedPaneFont != null) tabbedPane.setFont(tabbedPaneFont);
         splitPane2.setLeftComponent(tabbedPane);
         editorTab = new JPanel();
-        editorTab.setLayout(new GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
+        editorTab.setLayout(new GridLayoutManager(2, 1, new Insets(0, 0, 0, 0), -1, -1));
         Font editorTabFont = UIManager.getFont("TabbedPane.smallFont");
         if(editorTabFont != null) editorTab.setFont(editorTabFont);
         tabbedPane.addTab(this.$$$getMessageFromBundle$$$("i18n", "file.tab.untitled"), editorTab);
@@ -338,6 +338,14 @@ public class MainWindow extends View {
         Font editorFont = UIManager.getFont("EditorPane.font");
         if(editorFont != null) editor.setFont(editorFont);
         scrollPane1.setViewportView(editor);
+        final JPanel panel1 = new JPanel();
+        panel1.setLayout(new GridLayoutManager(1, 2, new Insets(0, 0, 0, 0), -1, -1));
+        editorTab.add(panel1, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        editorPositionLabel = new JLabel();
+        editorPositionLabel.setText(" ");
+        panel1.add(editorPositionLabel, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final Spacer spacer1 = new Spacer();
+        panel1.add(spacer1, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
         final JScrollPane scrollPane2 = new JScrollPane();
         splitPane2.setRightComponent(scrollPane2);
         memoryView = new JTable();
