0550c412f73d8e1ae35225a50a62efad0d2f0c55
Luca Gasperini <dev@xsoftware.it>
2023-02-05 19:10:04 +0100

    Split Memory getValueAt into getAddress and getData

--- src/main/java/it/uniroma2/pellegrini/z64sim/model/Memory.java
+++ src/main/java/it/uniroma2/pellegrini/z64sim/model/Memory.java
@@ -84,39 +84,47 @@ public class Memory extends AbstractTableModel {
     @Override
     public Object getValueAt(int row, int col) {
         if(col == 0) {
-            return String.format("%#016x", row * 8); // 64-bit address
+            return getAddress(row);
         } else if(col == 1) {
-            StringBuilder sb = new StringBuilder();
-            for(int i = 0; i < 8; i++) {
-                if(this.program == null) {
-                    sb.append("00 ");
-                } else {
-                    final MemoryElement memoryElement = this.program.getMemoryElementAt(row * 8L + i);
-                    if(memoryElement == null) {
-                        // Check for 16-byte instructions
-                        // TODO: all this should be made safer!
-                        final MemoryElement memoryElement2 = this.program.getMemoryElementAt((row - 1) * 8L);
-                        if(memoryElement2 instanceof InstructionClass1) {
-                            final OperandImmediate source = (OperandImmediate) ((InstructionClass1) memoryElement2).getSource();
-                            sb.append(source.toBytesString());
-                            i += 8;
-                        } else if(memoryElement2 instanceof InstructionClass2) {
-                            final OperandImmediate source = (OperandImmediate) ((InstructionClass2) memoryElement2).getSource();
-                            sb.append(source.toBytesString());
-                            i += 8;
-                        } else {
-                            sb.append("00 ");
-                        }
-                    } else {
-                        sb.append(memoryElement).append(" ");
-                        i += memoryElement.getSize() - 1;
-                    }
-                }
-            }
-            return sb.toString();
+            return getData(row);
         } else {
             throw new IllegalStateException("Internal error: unexpected column request");
         }
+    }
+
+    public String getAddress(int row) {
+        return String.format("%#016x", row * 8); // 64-bit address
+    }
+
+    public String getData(int row) {
+        if(this.program == null) {
+            return "00 00 00 00 00 00 00 00";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < 8; i++) {
+            final MemoryElement memoryElement = this.program.getMemoryElementAt(row * 8L + i);
+            if(memoryElement == null) {
+                // Check for 16-byte instructions
+                // TODO: all this should be made safer!
+                final MemoryElement memoryElement2 = this.program.getMemoryElementAt((row - 1) * 8L);
+                if(memoryElement2 instanceof InstructionClass1) {
+                    final OperandImmediate source = (OperandImmediate) ((InstructionClass1) memoryElement2).getSource();
+                    sb.append(source.toBytesString());
+                    i += 8;
+                } else if(memoryElement2 instanceof InstructionClass2) {
+                    final OperandImmediate source = (OperandImmediate) ((InstructionClass2) memoryElement2).getSource();
+                    sb.append(source.toBytesString());
+                    i += 8;
+                } else {
+                    sb.append("00 ");
+                }
+            } else {
+                sb.append(memoryElement).append(" ");
+                i += memoryElement.getSize() - 1;
+            }
+        }
+        return sb.toString();
     }
 
     @Override
